8feb4c6ed6c9d76729d24a14e822e4db67dea843
Yari <yaroslav.khudchenko@gmail.com>
2020-07-20 10:10:24 +0200

    setup app component, test for App component and css

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { App } from './../Components/App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("App exists", () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId(/App-test/).textContent).toBeTruthy();
 });
